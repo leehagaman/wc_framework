813acec6bcfa9c46cbdf186e5921b27cb12ca805
Lee Hagaman <hagamanlee@gmail.com>
2025-06-09 19:31:12 -0500

    added dirt to out-FV breakdown, added print statement to debug xf_sys segfault

--- wcp-uboone-bdt/apps/xf_cov_matrix.cxx
+++ wcp-uboone-bdt/apps/xf_cov_matrix.cxx
@@ -110,7 +110,11 @@ int main( int argc, char** argv )
   TMatrixD* cov_xf_mat = new TMatrixD(cov_add_mat->GetNrows(), cov_add_mat->GetNcols());
   TVectorD* vec_mean = new TVectorD(cov_add_mat->GetNrows());
 
+  std::cout << "starting cov.gen_xf_cov_matrix...\n";
+
   cov.gen_xf_cov_matrix(run, map_covch_hist, map_histoname_hist, vec_mean, cov_xf_mat);
+
+  std::cout << "done with cov.gen_xf_cov_matrix\n";
   
   TMatrixD* frac_cov_xf_mat = new TMatrixD(cov_add_mat->GetNrows(), cov_add_mat->GetNcols());
   for (size_t i=0; i!= frac_cov_xf_mat->GetNrows(); i++){
